b71a97f98b5b2fe4b547c3f04cb2e6ac12425e51
Mukesh Yadav <mackyonessolutions@gmail.com>
2026-04-14 22:52:34 +0530

    service worker enabled

--- src/index.js
+++ src/index.js
@@ -15,3 +15,11 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(() => console.log("SW registered"))
+      .catch((err) => console.log("SW error", err));
+  });
+}
